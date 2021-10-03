2cbcd77ef59c95a2c7e743dac8d58afe037e5c12
Asante Julius Kyere <asantejuliuskyere@gmail.com>
2021-10-03 03:49:53 +0000

    feat: Added account setup view

--- src/app/pages/auth/account-setup/account-setup.component.ts
+++ src/app/pages/auth/account-setup/account-setup.component.ts
@@ -7,6 +7,173 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSetupComponent implements OnInit {
 
+  // json data of all countries in Africa
+  countries = [
+      {
+        "name": "Algeria"
+      },
+      {
+        "name": "Angola"
+      },
+      {
+        "name": "Benin"
+      },
+      {
+        "name": "Botswana"
+      },
+      {
+        "name": "Burkina Faso"
+      },
+      {
+        "name": "Burundi"
+      },
+      {
+        "name": "Cabo Verde"
+      },
+      {
+        "name": "Cameroon"
+      },
+      {
+        "name": "Central African Republic"
+      },
+      {
+        "name": "Chad"
+      },
+      {
+        "name": "Comoros"
+      },
+      {
+        "name": "Democratic Republic of the Congo"
+      },
+      {
+        "name": "Republic of the Congo"
+      },
+      {
+        "name": "Cote D'Ivoire"
+      },
+      {
+        "name": "Djibouti"
+      },
+      {
+        "name": "Egypt"
+      },
+      {
+        "name": "Equatorial Guinea"
+      },
+      {
+        "name": "Eritrea"
+      },
+      {
+        "name": "Ethiopia"
+      },
+      {
+        "name": "Gabon"
+      },
+      {
+        "name": "Ghana"
+      },
+      {
+        "name": "Gambia"
+      },
+      {
+        "name": "Guinea"
+      },
+      {
+        "name": "Guinea-Bissau"
+      },
+      {
+        "name": "Kenya"
+      },
+      {
+        "name": "Lesotho"
+      },
+      {
+        "name": "Liberia"
+      },
+      {
+        "name": "Libya"
+      },
+      {
+        "name": "Madagascar"
+      },
+      {
+        "name": "Malawi"
+      },
+      {
+        "name": "Mali"
+      },
+      {
+        "name": "Mauritania"
+      },
+      {
+        "name": "Mauritius"
+      },
+      {
+        "name": "Morocco"
+      },
+      {
+        "name": "Mozambique"
+      },
+      {
+        "name": "Namibia"
+      },
+      {
+        "name": "Niger"
+      },
+      {
+        "name": "Nigeria"
+      },
+      {
+        "name": "Rwanda"
+      },
+      {
+        "name": "Sao Tome And Principe"
+      },
+      {
+        "name": "Senegal"
+      },
+      {
+        "name": "Seychelles"
+      },
+      {
+        "name": "Sierra Leone"
+      },
+      {
+        "name": "Somalia"
+      },
+      {
+        "name": "South Africa"
+      },
+      {
+        "name": "South Sudan"
+      },
+      {
+        "name": "Sudan"
+      },
+      {
+        "name": "Eswatini"
+      },
+      {
+        "name": "Tanzania"
+      },
+      {
+        "name": "Togo"
+      },
+      {
+        "name": "Tunisia"
+      },
+      {
+        "name": "Uganda"
+      },
+      {
+        "name": "Zambia"
+      },
+      {
+        "name": "Zimbabwe"
+      }
+
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api/v1/',
+  // apiUrl: 'http://localhost:8000/api/v1/',
+  apiUrl: 'https://safiri.juliusasante.com/api/v1/',
 };
 
 /*
